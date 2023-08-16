26a8f0527235ba667fd9e20b23bde821dfae3ea5
Vadim Elsukov <elsukov.vadim@gmail.com>
2023-08-16 10:02:35 +0300

    fix: remove comments

--- backend/app.js
+++ backend/app.js
@@ -34,35 +34,6 @@ const limiter = rateLimit({
   max: 100,
 });
 
-/*
-const allowedCors = [
-  'https://hvny-web.students.nomoreparties.co',
-  'http://hvny-web.students.nomoreparties.co',
-  'https://api.hvny-web.students.nomoreparties.co',
-  'http://api.hvny-web.students.nomoreparties.co',
-  'http://localhost:3001',
-  'http://localhost:3000',
-];
-
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const reqHeaders = req.headers['access-control-request-headers'];
-
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', reqHeaders);
-    return res.end();
-  }
-
-  return next();
-});
-*/
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
